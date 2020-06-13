b628168cf5430618b75e6b723a2e56ddee3d872d
sofia819 <sofia819@gmail.com>
2020-06-13 02:52:06 -0400

    update to q41

--- 1-100/41.py
+++ 1-100/41.py
@@ -1,37 +1,37 @@
 """
 41. First Missing Positive
 https://leetcode.com/problems/first-missing-positive/
-Using this solution: https://www.youtube.com/watch?v=2QugZILS_Q8
+Using this solution: https://www.youtube.com/watch?v=9SnkdYXNIzM
 """
 
 
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        # check if 1 exists
         oneExists = False
         for i in nums:
             if i == 1:
                 oneExists = True
-
         if not oneExists:
             return 1
 
+        # change negatives and num > len(nums) to 1 to ignore them
         for i, val in enumerate(nums):
-            if val <= 0 or val > len(nums):
+            if val <= 0 or val > n:
                 nums[i] = 1
 
-        for i in range(len(nums)):
-            iAbs = abs(nums[i])
+        for i in nums:
+            iAbs = abs(i)
+            if nums[iAbs - 1] > 0:
+                nums[iAbs - 1] *= -1
 
-            if iAbs < len(nums):
-                nums[iAbs] = -1 * abs(nums[iAbs])
-            else:
-                nums[0] = -1 * abs(nums[0])
-
-        for i in range(1, len(nums)):
+        for i in range(0, len(nums)):
             if nums[i] > 0:
-                return i
+                return i + 1
 
         if nums[0] > 0:
-            return len(nums)
+            return n
 
-        return len(nums) + 1
+        return n + 1
