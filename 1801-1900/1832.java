247ab1231f6fdac22917363d5c9aefa44658ba79
Sofia Lee <sofia819@gmail.com>
2021-05-08 00:49:36 -0400

    1832

---
+++ 1801-1900/1832.java
@@ -0,0 +1,15 @@
+/**
+ * 1832. Check if the Sentence Is Pangram
+ * https://leetcode.com/problems/check-if-the-sentence-is-pangram/
+ */
+
+class Solution {
+
+  public boolean checkIfPangram(String sentence) {
+    Set<Character> s = new HashSet<>();
+    for (int i = 0; i < sentence.length(); i++) {
+      s.add(sentence.charAt(i));
+    }
+    return s.size() == 26;
+  }
+}
